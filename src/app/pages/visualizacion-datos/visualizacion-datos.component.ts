31bf6108b7c9cc2a64312d22ab80cff1fe4b7983
reyan <reyandres015@gmail.com>
2024-04-01 20:48:37 -0500

    Visualización de datos por galpón (Ventas - Gastos)
    -Faltan agregar las graficas.
    -Ajuste visual

--- src/app/pages/visualizacion-datos/visualizacion-datos.component.ts
+++ src/app/pages/visualizacion-datos/visualizacion-datos.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./visualizacion-datos.component.scss']
 })
 export class VisualizacionDatosComponent {
+  galpones: string[] = [];
+  galpon: string = '';
 
+  nombreGranja: string = "no data";
+  nombreGalpon: string = "no data";
+
+  ventasTotales: string = '0';
+
+  constructor() { }
+
+  arrowBack() {
+    window.history.back()
+  }
+
+  moneyFormat(money: string) {
+    const translateMoney = parseInt(money)
+    return translateMoney.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0
+    });
+  }
 }
