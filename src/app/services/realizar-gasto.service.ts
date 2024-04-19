6d1d842ad11190018b3dca385cd96e9fcd01c902
reyan <reyandres015@gmail.com>
2024-04-19 08:05:53 -0500

    Gastos: Configuraciones de vista y conexión a base de datos implementada

---
+++ src/app/services/realizar-gasto.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import Gastos from '../interfaces/gastos.interface';
+import { GalponDataService } from './galpon-data.service';
+import { GetDataFirebaseService } from './get-data-firebase.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RealizarGastoService {
+
+  constructor(
+    private galponDataService: GalponDataService,
+    private getDataFirebase: GetDataFirebaseService
+  ) { }
+
+  gastos: Gastos[] = [];
+
+  async registrarGasto(gasto: Gastos) {
+    this.gastos.push(gasto);
+    let gastosGalpon = this.galponDataService.getGalpon().gastos;
+    if (gastosGalpon) {
+      gastosGalpon.push(gasto);
+    } else {
+      this.galponDataService.getGalpon().gastos = [gasto];
+    }
+    await this.updateVenta();
+  }
+
+  // Realiza la ejecucion de los update para todos los documentos de ventas pendientes por subir. (No internet conection)
+  async updateVenta() {
+    const refColeccionGalpon = this.galponDataService.getGalpon().ref + '/gastos';
+    for (let i = 0; i < this.gastos.length; i++) {
+      await this.getDataFirebase.createUpdateDoc(refColeccionGalpon, this.gastos[i]);
+    }
+  }
+}
